c5e945d9e1b1910c979b5951d1d34d4b4708bc3e
blockapicoder <blockapicoder@gmail.com>
2025-10-11 14:57:24 +0200

    dev

---
+++ index.ts
@@ -0,0 +1,42 @@
+import * as test from "./dist/test.ts"
+import * as api from './dist/api.ts'
+test.test("Test read file", async () => {
+
+    const client = api.createClient();
+    const txt = "hello.world"
+    await client.writeFileText("test.txt", txt)
+    let rep = await client.explorer({})
+    test.assertEquals(rep.type === "directory", true)
+    if (rep.type === "directory") {
+
+        test.assertEquals(rep.content.some((e) => {
+            if (e.type !== "directory") {
+                return e.name === "test.txt"
+            }
+            return false
+
+        }), true, "pas dans rep")
+    }
+
+    const r = await client.readFileText("test.txt")
+
+    test.assertEquals(r, txt)
+
+    await client.deleteFile("test.txt")
+    rep = await client.explorer({})
+    test.assertEquals(rep.type === "directory", true)
+    if (rep.type === "directory") {
+
+        test.assertEquals(rep.content.some((e) => {
+            if (e.type !== "directory") {
+                return e.name === "test.txt"
+            }
+            return false
+
+        }), false, "pas dans rep")
+    }
+
+
+
+
+})
